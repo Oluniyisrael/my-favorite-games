f7c918bc68c2bd24d8d919c69db9f3f42d97eb3c
Oluniyisrael <israeloluniyi2004@gmail.com>
2023-12-05 16:45:18 +0100

    Test 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,40 @@
+import React, { useState } from "react";
 import "./App.css";
 import { characters } from "./MarvelData";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredCharacters = characters.filter((data) =>
+    data.alias.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="mainBody">
       <div className="topHeader">
         <h1 className="header">Israel's games Catalogue</h1>
-        <input placeholder="Search a Marvel Character" className="topInput" />
+        <input
+          placeholder="Search a Marvel Character"
+          className="topInput"
+          value={searchTerm}
+          onChange={handleSearch}
+        />
       </div>
       <div className="cardBody">
-        {characters.map((data, index) => (
+        {filteredCharacters.map((data, index) => (
           <div key={index} className="card">
             <div className="main">
-              <img className="avatarImage" src={data.img1} alt={data.alias} />
+              <img
+                className="avatarImage"
+                src={data.img1}
+                alt={data.alias}
+              />
               <h2>{data.alias}</h2>
-              <p className="description">
-                {data?.abilities[0]}
-              </p>
+              <p className="description">{data?.abilities[0]}</p>
               <div className="avatarInfo">
                 <div className="price">
                   <ins>◘</ins>
